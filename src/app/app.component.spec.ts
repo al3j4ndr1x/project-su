a5c3af5b27cb275f9ef81a4a59dda9c144ba51f1
Al3j4ndr1x <50196531+al3j4ndr1x@users.noreply.github.com>
2019-06-18 05:55:08 -0400

    New title, description, and button.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-su'`, () => {
+  it(`should have as title 'The Project SU'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('project-su');
+    expect(app.title).toEqual('The Project SU');
   });
 
   it('should render title in a h1 tag', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'project-su';
+  title = 'The Project SU';
 }
