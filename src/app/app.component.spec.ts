1de04fb1ddb80112c668447e04052b248f19eb42
Al3j4ndr1x <50196531+al3j4ndr1x@users.noreply.github.com>
2019-06-18 07:10:50 -0400

    fixed tests with nebular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,19 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { NbThemeModule, NbSidebarModule, NbLayoutModule, NbButtonModule, NbInputModule, NbCardModule, } from '@nebular/theme';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        NbThemeModule.forRoot({ name: 'cosmic' }),
+        NbLayoutModule,
+        NbSidebarModule.forRoot(),
+        NbButtonModule,
+        NbInputModule,
+        NbCardModule,
       ],
       declarations: [
         AppComponent
@@ -30,6 +38,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to project-su!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to The Project SU!');
   });
 });
